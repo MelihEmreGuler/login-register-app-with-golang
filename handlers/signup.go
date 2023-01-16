adeacbe4a346e2a89715ce40f2df1d1eabac3a99
MelihEmreGuler <melihemreguler@hotmail.com>
2023-01-16 21:31:26 +0300

    user signup process completed

---
+++ handlers/signup.go
@@ -0,0 +1,64 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/MelihEmreGuler/login-register-app-with-golang/helpers"
+)
+
+func Run() {
+	http.HandleFunc("/", Index)
+	http.HandleFunc("/login", Login)
+	http.HandleFunc("/signup", Signup)
+	http.ListenAndServe(":8080", nil)
+}
+
+func Signup(w http.ResponseWriter, r *http.Request) {
+	uName, email, psw, pwdCnfrm := "", "", "", ""
+	r.ParseForm()
+
+	uName = r.FormValue("username")
+	email = r.FormValue("email")
+	psw = r.FormValue("password")
+	pwdCnfrm = r.FormValue("confirm")
+
+	if CheckFormValues(uName, email, psw, pwdCnfrm, w) {
+		fmt.Fprintf(w, "%s's Registration successful \n", uName)
+		ShowUserData(uName, email, psw, pwdCnfrm, w)
+	} else {
+		fmt.Fprintf(w, "Registration failed \n")
+		return
+	}
+
+}
+
+func CheckFormValues(uName, email, psw, pwdCnfrm string, w http.ResponseWriter) bool {
+	uNameCheck := helpers.IsEmpty(uName)
+	emailCheck := helpers.IsEmpty(email)
+	pswCheck := helpers.IsEmpty(psw)
+	pwdCnfrmCheck := helpers.IsEmpty(pwdCnfrm)
+
+	if uNameCheck || emailCheck || pswCheck || pwdCnfrmCheck {
+		fmt.Fprintf(w, "Please fill all the fields \n")
+		return false
+	}
+	if psw != pwdCnfrm {
+		fmt.Fprintf(w, "Passwords do not match \n")
+		return false
+	}
+	return true
+}
+
+func ShowUserData(uName, email, psw, pwdCnfrm string, w http.ResponseWriter) {
+
+	fmt.Fprintf(w, "Username: %s \n", uName)
+	fmt.Fprintf(w, "Email: %s \n", email)
+	fmt.Fprintf(w, "Password: %s \n", psw)
+	fmt.Fprintf(w, "Password Confirmation: %s \n", pwdCnfrm)
+
+}
+
+// for key, value := range r.Form {
+// 	fmt.Println("key:", key, "value:", value)
+// }
